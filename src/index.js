ae1b5eb6a0c9b867b007ae8a62367046ba6d18b8
UgnisSoftware <karolis.masiulis@ugnis.com>
2015-07-15 15:08:38 +0300

    Fixing IE9 bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require("babel").transform("code", { optional: ["runtime"] });
+
 exports.SubLink = require('./SubLink');
 exports.SubRoute = require('./SubRoute');
 exports.SubRouter = require('./SubRouter');
